85fc76da399153dc69c1f8227157949fa182443d
zonika <zchodosh@conncoll.edu>
2015-09-15 17:42:52 -0400

    done

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -7,7 +7,5 @@ class PicturesController < ApplicationController
   def like
     @picture = Picture.find(params[:id])
     @picture.update_likes(current_user)
-    redirect_to root_path
   end
-
 end
